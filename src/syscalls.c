ce79415d68c44d46fc4a10eae6a5bed72abf0276
Hugo Girard <hugo.girard@epita.fr>
2023-07-16 19:03:04 +0200

    add all syscalls

--- src/syscalls.c
+++ src/syscalls.c
@@ -1,5 +1,6 @@
 #include "syscalls.h"
 
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,18 +10,18 @@
 
 extern struct global *glob;
 
-void print_int()
+void print_int(void)
 {
   printf("%d\n", (int32_t)glob->reg[A0]);
 }
 
-void print_string()
+void print_string(void)
 {
   char *value = ((char *)glob->memory) + glob->reg[A0];
   printf("%s", value);
 }
 
-void read_int()
+void read_int(void)
 {
   int value = 0;
   scanf("%d", &value);
@@ -28,7 +29,7 @@ void read_int()
   glob->reg[V0] = value;
 }
 
-void read_string()
+void read_string(void)
 {
   char *buf = calloc(glob->reg[A1], sizeof(char));
   ssize_t ret = read(STDIN_FILENO, buf, glob->reg[A1]);
@@ -42,7 +43,40 @@ void read_string()
   free(buf);
 }
 
-int call_syscall()
+void print_char(void)
+{
+  printf("%c\n", glob->reg[A0]);
+}
+
+void read_char(void)
+{
+  char value = 0;
+  scanf("%c", &value);
+  glob->reg[V0] = value;
+}
+
+void _open(void)
+{
+  glob->reg[V0] = open((char *) glob->memory + glob->reg[A0], glob->reg[A1]);
+}
+
+void _read(void)
+{
+  glob->reg[V0] = read(glob->reg[A0], (char *) glob->memory + glob->reg[A1], glob->reg[A2]);
+}
+
+
+void _write(void)
+{
+  glob->reg[V0] = write(glob->reg[A0], (char *) glob->memory + glob->reg[A1], glob->reg[A2]);
+}
+
+void _close(void)
+{
+  close(glob->reg[A0]);
+}
+
+int call_syscall(void)
 {
   uint32_t syscall_value = glob->reg[V0];
   switch (syscall_value)
@@ -61,6 +95,26 @@ int call_syscall()
     break;
   case 10:
     return 1;
+  case 11:
+    print_char();
+    break;
+  case 12:
+    read_char();
+    break;
+  case 13:
+    _open();
+    break;
+  case 14:
+    _read();
+    break;
+  case 15:
+    _write();
+    break;
+  case 16:
+    _close();
+    break;
+  case 17:
+    return glob->reg[A0];
   }
   return 0;
 }
