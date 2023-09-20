739fb17941ffc5482ec393004d435c812d8ad60a
Hugo Girard <hugo.girard@epita.fr>
2023-09-20 16:25:34 +0200

    log to stderr instead of stdout

--- src/logger.c
+++ src/logger.c
@@ -15,86 +15,86 @@ void log_reg_instr(uint32_t *instru)
   uint32_t rt = ((*instru) >> 16) & 0x1F;
   uint32_t rd = ((*instru) >> 11) & 0x1F;
   uint32_t sa = ((*instru) >> 6) & 0x1F;
-  printf("[my_mips] Executing pc = %08x: %08x: ", glob->pc, *instru);
+  fprintf(stderr, "[my_mips] Executing pc = %08x: %08x: ", glob->pc, *instru);
   switch (function)
   {
   case ADD:
-    printf("add r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "add r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case ADDU:
-    printf("addu r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "addu r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case DIV:
-    printf("div r%d, r%d\n", rs, rt);
+    fprintf(stderr, "div r%d, r%d\n", rs, rt);
     break;
   case DIVU:
-    printf("divu r%d, r%d\n", rs, rt);
+    fprintf(stderr, "divu r%d, r%d\n", rs, rt);
     break;
   case MULT:
-    printf("mult r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "mult r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case MULTU:
-    printf("multu r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "multu r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case SUB:
-    printf("sub r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "sub r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case SUBU:
-    printf("subu r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "subu r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case AND:
-    printf("and r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "and r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case NOR:
-    printf("nor r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "nor r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case OR:
-    printf("or r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "or r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case SLT:
-    printf("slt r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "slt r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case SLTU:
-    printf("sltu r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "sltu r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case XOR:
-    printf("xor r%d, r%d, r%d\n", rd, rs, rt);
+    fprintf(stderr, "xor r%d, r%d, r%d\n", rd, rs, rt);
     break;
   case SLL:
-    printf("sll r%d, r%d, r%d\n", rd, rt, sa);
+    fprintf(stderr, "sll r%d, r%d, r%d\n", rd, rt, sa);
     break;
   case SLLV:
-    printf("sllv r%d, r%d, r%d\n", rd, rt, rs);
+    fprintf(stderr, "sllv r%d, r%d, r%d\n", rd, rt, rs);
     break;
   case SRA:
-    printf("sra r%d, r%d, r%d\n", rd, rt, sa);
+    fprintf(stderr, "sra r%d, r%d, r%d\n", rd, rt, sa);
     break;
   case SRAV:
-    printf("srav r%d, r%d, r%d\n", rd, rt, rs);
+    fprintf(stderr, "srav r%d, r%d, r%d\n", rd, rt, rs);
     break;
   case SRL:
-    printf("srl r%d, r%d, r%d\n", rd, rt, sa);
+    fprintf(stderr, "srl r%d, r%d, r%d\n", rd, rt, sa);
     break;
   case SRLV:
-    printf("srlv r%d, r%d, r%d\n", rd, rt, rs);
+    fprintf(stderr, "srlv r%d, r%d, r%d\n", rd, rt, rs);
     break;
   case JALR:
-    printf("jalr r%d, r%d\n", rd, rs);
+    fprintf(stderr, "jalr r%d, r%d\n", rd, rs);
     break;
   case JR:
-    printf("jr r%d\n", rs);
+    fprintf(stderr, "jr r%d\n", rs);
     break;
   case MFHI:
-    printf("mfhi r%d\n", rd);
+    fprintf(stderr, "mfhi r%d\n", rd);
     break;
   case MFLO:
-    printf("mflo r%d\n", rd);
+    fprintf(stderr, "mflo r%d\n", rd);
     break;
   case MTHI:
-    printf("mthi r%d\n", rs);
+    fprintf(stderr, "mthi r%d\n", rs);
     break;
   case MTLO:
-    printf("mtlo r%d\n", rs);
+    fprintf(stderr, "mtlo r%d\n", rs);
     break;
   default:
     perror("Bad register instruction\n");
@@ -108,98 +108,98 @@ void log_imm_instr(uint32_t *instru)
   uint32_t rs = ((*instru) >> 21) & 0x1F;
   uint32_t rt = ((*instru) >> 16) & 0x1F;
   uint32_t imm = (*instru) & 0xFFFF;
-  printf("[my_mips] Executing pc = %08x: %08x: ", glob->pc, *instru);
+  fprintf(stderr, "[my_mips] Executing pc = %08x: %08x: ", glob->pc, *instru);
   if (opcode == 1)
   {
     switch (rt)
     {
     case BGEZ:
-      printf("bgez r%d, %x\n", rs, imm);
+      fprintf(stderr, "bgez r%d, %x\n", rs, imm);
       break;
     case BGEZAL:
-      printf("bgezal r%d, %x\n", rs, imm);
+      fprintf(stderr, "bgezal r%d, %x\n", rs, imm);
       break;
     case BLTZ:
-      printf("bltz r%d, %x\n", rs, imm);
+      fprintf(stderr, "bltz r%d, %x\n", rs, imm);
       break;
     case BLTZAL:
-      printf("bltz r%d, %x\n", rs, imm);
+      fprintf(stderr, "bltz r%d, %x\n", rs, imm);
       break;
     }
   }
   switch (opcode)
   {
   case ADDI:
-    printf("addi r%d, r%d, %x\n", rt, rs, imm);
+    fprintf(stderr, "addi r%d, r%d, %x\n", rt, rs, imm);
     break;
   case ADDIU:
-    printf("addiu r%d, r%d, %x\n", rt, rs, imm);
+    fprintf(stderr, "addiu r%d, r%d, %x\n", rt, rs, imm);
     break;
   case ANDI:
-    printf("andi r%d, r%d, %x\n", rt, rs, imm);
+    fprintf(stderr, "andi r%d, r%d, %x\n", rt, rs, imm);
     break;
   case ORI:
-    printf("ori r%d, r%d, %x\n", rt, rs, imm);
+    fprintf(stderr, "ori r%d, r%d, %x\n", rt, rs, imm);
     break;
   case XORI:
-    printf("xori r%d, r%d, %x\n", rt, rs, imm);
+    fprintf(stderr, "xori r%d, r%d, %x\n", rt, rs, imm);
     break;
   case SLTI:
-    printf("slti r%d, r%d, %x\n", rt, rs, imm);
+    fprintf(stderr, "slti r%d, r%d, %x\n", rt, rs, imm);
     break;
   case SLTIU:
-    printf("sltiu r%d, r%d, %x\n", rt, rs, imm);
+    fprintf(stderr, "sltiu r%d, r%d, %x\n", rt, rs, imm);
     break;
   case BEQ:
-    printf("beq r%d, r%d, %x\n", rs, rt, imm);
+    fprintf(stderr, "beq r%d, r%d, %x\n", rs, rt, imm);
     break;
   case BNE:
-    printf("bne r%d, r%d, %x\n", rs, rt, imm);
+    fprintf(stderr, "bne r%d, r%d, %x\n", rs, rt, imm);
     break;
   case BGTZ:
-    printf("bgtz r%d, %x\n", rs, imm);
+    fprintf(stderr, "bgtz r%d, %x\n", rs, imm);
     break;
   case BLEZ:
-    printf("blez r%d, %x\n", rs, imm);
+    fprintf(stderr, "blez r%d, %x\n", rs, imm);
     break;
   case LB:
-    printf("lb r%d, %x\n", rt, imm);
+    fprintf(stderr, "lb r%d, %x\n", rt, imm);
     break;
   case LBU:
-    printf("lbu r%d, %x\n", rt, imm);
+    fprintf(stderr, "lbu r%d, %x\n", rt, imm);
     break;
   case LH:
-    printf("lh r%d, %x\n", rt, imm);
+    fprintf(stderr, "lh r%d, %x\n", rt, imm);
     break;
   case LHU:
-    printf("lhu r%d, %x\n", rt, imm);
+    fprintf(stderr, "lhu r%d, %x\n", rt, imm);
     break;
   case LW:
-    printf("lw r%d, %x\n", rt, imm);
+    fprintf(stderr, "lw r%d, %x\n", rt, imm);
     break;
   case LWL:
-    printf("lwl r%d, %x\n", rt, imm);
+    fprintf(stderr, "lwl r%d, %x\n", rt, imm);
     break;
   case LWR:
-    printf("lwr r%d, %x\n", rt, imm);
+    fprintf(stderr, "lwr r%d, %x\n", rt, imm);
     break;
   case SB:
-    printf("sb r%d, %x\n", rt, imm);
+    fprintf(stderr, "sb r%d, %x\n", rt, imm);
     break;
   case SH:
-    printf("sh r%d, %x\n", rt, imm);
+    fprintf(stderr, "sh r%d, %x\n", rt, imm);
     break;
   case SW:
-    printf("sw r%d, %x\n", rt, imm);
+    fprintf(stderr, "sw r%d, %x\n", rt, imm);
     break;
   case SWL:
-    printf("swl r%d, %x\n", rt, imm);
+    fprintf(stderr, "swl r%d, %x\n", rt, imm);
     break;
   case SWR:
-    printf("swr r%d, %x\n", rt, imm);
+    fprintf(stderr, "swr r%d, %x\n", rt, imm);
     break;
   case LUI:
-    printf("lui r%d, %x\n", rt, imm);
+    fprintf(stderr, "lui r%d, %x\n", rt, imm);
     break;
   default:
     perror("Bad immediate instruction\n");
--- test/sub.s
+++ test/sub.s
@@ -2,8 +2,12 @@
 
 .global main
 main:
-    ori $t0, $0, 0x42
-    ori $t1, $0, 0x24
-    sub $a0, $t1, $t0
+    ori $t0, $0, 0xFFFF
+    ori $t1, $0, 0xFFFF
+    #sub $a0, $t1, $t0
+    xor $a0, $a0
+    add $a0, $t0, $t1
+    add $a0, $a0, $t1
+    sll $a0, $a0, 26
     jal print_int
     j exit
