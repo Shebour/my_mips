5351584cc9234c3644d28056afde04d17a4c49d2
Shebour <shebour95@gmail.com>
2026-06-14 12:10:56 +0200

    update tests

--- coverage.sh
+++ coverage.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-gcov -o build/bin src/*.c
-gcovr --html-details -o index.html
+rm -rf coverage
+mkdir coverage
 
+make clean
+make coverage
+make test
+
+python3 test_suite.py
+gcov -o build/bin src/*.c
+gcovr --html-details -o coverage/index.html
+
+firefox coverage/index.html
--- test_suite.py
+++ test_suite.py
@@ -1,41 +1,50 @@
 #! /usr/bin/env python3
 
-import yaml
+import json
 import subprocess
-from yaml.loader import SafeLoader
 from colorama import Fore
 
-test_file = "test/test.yaml"
+test_file = "test/test.json"
 binary = "build/bin/my_mips"
 
 with open(test_file, 'r') as f:
-    data = yaml.load(f, Loader=SafeLoader)
+    data = json.load(f)
 if not data:
     print(f"Failed to read {test_file}")
     exit(2)
 
-
 passed = 0
 total = 0
 
-for test in data:
-    total += 1
-    filename = test['file']
-    output = test['output']
-    print(f"{filename}")
+def execute_test(test_data):
+    param = test_data['param']
+    expected : str = test_data['expected']
+    if "{binary_name}" in expected:
+        expected = expected.replace("{binary_name}", binary)
+    print(f"=== {test_data['name']} ===")
     try:
-        proc = subprocess.run([binary, "test/" + filename], capture_output=True,
-                              text=True, timeout=2)
+        if param == None:
+            proc = subprocess.run([binary], capture_output=True, text=True, timeout=2)
+        else:
+            proc = subprocess.run([binary, "test/" + param], capture_output=True,
+                                text=True, timeout=2)
         exit_status = proc.returncode
         out_proc = proc.stdout
         err_proc = proc.stderr
     except subprocess.TimeoutExpired:
         print(Fore.RED + "Timeout" + Fore.RESET)
-        continue
-    if out_proc != output:
-        print(f"{Fore.RED}KO{Fore.RESET}: {filename}")
-        print(f"Got: {out_proc}\nExpected: {output}")
+        return False
+    if out_proc != expected and err_proc != expected:
+        print(f"{Fore.RED}KO{Fore.RESET}: {param}")
+        print(f"Got: {out_proc}\nExpected: {expected}")
+        return False
     else:
         print(f"{Fore.GREEN}OK{Fore.RESET}")
+        return True
+    
+
+for test_data in data["Tests"]:
+    total += 1
+    if execute_test(test_data):
         passed += 1
 print(f"{passed}/{total}")
